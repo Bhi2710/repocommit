8ce4bb4212028d5ffb1b1375433599074295dbd0
Abhishek Yadav <92436824+Bhi2710@users.noreply.github.com>
2023-03-11 10:51:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Detail from './components/Detail';
-
+import Header from './components/Header';
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <Detail />
     </div>
   );
